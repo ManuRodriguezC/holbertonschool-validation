dd8bb19fb6f3320b883f54fe3e37e09bf0bf23d1
ManuRodriguezC <estebanclimb@gmail.com>
2023-01-13 12:37:44 -0500

    Changes in the script

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-apt-get update && apt-get install -y hugo make
+#!/bin/bash
+apt-get update && apt-get install -y make hugo
 make build
