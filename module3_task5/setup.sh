d7bdbffd02b16e9baecab20b46944a0cca2a4975
ManuRodriguezC <estebanclimb@gmail.com>
2023-01-16 13:05:09 -0500

    Task5

---
+++ module3_task5/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
+apt-get update && apt-get install -y hugo make
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
+apt install ./last.deb
+rm last.deb
