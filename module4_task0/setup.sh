63fbc8bc85c469aa8eff5094a48a618a15f579eb
ManuRodriguezC <estebanclimb@gmail.com>
2023-01-16 14:12:05 -0500

    Task 0 module 4

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-docker run golang:1.15.8-buster go --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-apt-get update && apt-get install -y hugo make
+rm -rf /usr/local/go
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
 apt install ./last.deb
 rm last.deb
