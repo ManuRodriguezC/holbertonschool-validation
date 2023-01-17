972a1d7c01dc77d1e479fdb0a7ce7e7195ed3612
ManuRodriguezC <estebanclimb@gmail.com>
2023-01-16 19:13:04 -0500

    Update task 1

--- module4_task1/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf /usr/local/go
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
-apt install ./last.deb
-rm last.deb
