1b5973bf2bd4765d82e49d1fcaf92c0b383d2474
ManuRodriguezC <estebanclimb@gmail.com>
2023-01-23 18:43:38 -0500

    Update task 4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
 apt update && apt install -y make hugo
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 apt install ./hugo/deb
