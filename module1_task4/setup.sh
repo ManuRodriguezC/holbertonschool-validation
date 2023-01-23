a6f90e5683f5db12290bc3289d470410f454d32e
ManuRodriguezC <estebanclimb@gmail.com>
2023-01-23 18:46:46 -0500

    Update task 4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt update && apt install -y make hugo
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-apt install ./hugo/deb
+apt install ./hugo.deb
 rm hugo.deb
 make build
